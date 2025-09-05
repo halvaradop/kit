c6c477a32a7ada0badf7e99f365dfa7d9de92ebe
Hernan Alvarado <hernanvid123@gmail.com>
2025-09-05 15:25:47 -0500

    chore(eslint): update `ESLint` config

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,8 @@
 import { baseConfig } from "@halvaradop/eslint-config/base"
-import { nextConfig } from "@halvaradop/eslint-config/next"
+import { nextConfig } from "@halvaradop/eslint-config/nextjs"
 import { reactConfig } from "@halvaradop/eslint-config/react"
-import { turboConfig } from "@halvaradop/eslint-config/turbo"
+import { turborepoConfig } from "@halvaradop/eslint-config/turbo"
 import { prettierConfig } from "@halvaradop/eslint-config/prettier"
-import { typescriptConfig } from "@halvaradop/eslint-config/typescript"
+import { tsConfig } from "@halvaradop/eslint-config/typescript"
 
-export default [...baseConfig, ...nextConfig, ...reactConfig, ...turboConfig, ...prettierConfig, ...typescriptConfig]
+export default [...baseConfig, ...nextConfig, ...reactConfig, ...turborepoConfig, ...prettierConfig, ...tsConfig]
